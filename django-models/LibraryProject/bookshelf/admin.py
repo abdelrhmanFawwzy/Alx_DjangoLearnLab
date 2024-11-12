15c3bd140106c3bab25d23da7fc759f0c918a44b
abdelrhmanFawwzy <110397608+abdelrhmanFawwzy@users.noreply.github.com>
2024-11-12 11:03:18 +0200

    Add files via upload

---
+++ django-models/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Book
+
+# Register your models here.
+admin.site.register(Book)
+admin.ModelAdmin
+["list_filter", "author", "publication_year"]
+["search_fields", "title"]
