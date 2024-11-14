b209c494a564dd9e34a21dfa76be96512f11b0eb
abdelrhmanFawwzy <110397608+abdelrhmanFawwzy@users.noreply.github.com>
2024-11-14 16:54:02 +0200

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,2 +1,3 @@
 from .views import list_books", "LibraryDetailView", "path
 views.register", "LogoutView.as_view(template_name=", "LoginView.as_view(template_name="
+["add_book/", "edit_book/", "delete_book"]
