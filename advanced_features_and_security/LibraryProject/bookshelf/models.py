6d371946299eab0110939600ab99894777b96780
abdelrhmanFawwzy <110397608+abdelrhmanFawwzy@users.noreply.github.com>
2024-11-14 17:05:04 +0200

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,3 +1,4 @@
 
 ["class CustomUser(AbstractUser):", "date_of_birth", "profile_photo"]
 ["class CustomUserManager(BaseUserManager):", "create_user", "create_superuser"]
+["class Book(models.Model):", "can_create", "can_delete"]
