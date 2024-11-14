75fe96b7492584383f649cabbe7bc24c650893b7
abdelrhmanFawwzy <110397608+abdelrhmanFawwzy@users.noreply.github.com>
2024-11-14 16:46:06 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,4 @@
 relationship_app/list_books.html", "Book.objects.all()
 relationship_app/library_detail.html", "library", "from .models import Library"
 from django.views.generic.detail import DetailView
+"from django.contrib.auth import login", "from django.contrib.auth.forms import UserCreationForm"
