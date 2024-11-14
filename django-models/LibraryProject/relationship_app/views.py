7f25f97e8b4688af2d66b680d26a48eff9534afe
abdelrhmanFawwzy <110397608+abdelrhmanFawwzy@users.noreply.github.com>
2024-11-14 16:53:38 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,3 +2,4 @@ relationship_app/list_books.html", "Book.objects.all()
 relationship_app/library_detail.html", "library", "from .models import Library"
 from django.views.generic.detail import DetailView
 "from django.contrib.auth import login", "from django.contrib.auth.forms import UserCreationForm"
+["from django.contrib.auth.decorators import permission_required", "relationship_app.can_add_book", "relationship_app.can_change_book", "relationship_app.can_delete_book"]
