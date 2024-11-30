f3173a0394769cdd30602629dfc16297bcde9bbc
abdelrhmanFawwzy <110397608+abdelrhmanFawwzy@users.noreply.github.com>
2024-11-30 13:34:10 +0200

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,2 +1,3 @@
 ["ListView", "DetailView", "CreateView", "UpdateView", "DeleteView"]
 ["from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated", "IsAuthenticated"]
+["from django_filters import rest_framework", "from rest_framework import generics"]
