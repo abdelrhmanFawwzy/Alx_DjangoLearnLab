b1786b42794e0faaa8fe3e37a91b56a8604a368a
abdelrhmanFawwzy <110397608+abdelrhmanFawwzy@users.noreply.github.com>
2024-12-08 21:18:27 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,6 +1,7 @@
 "post/<int:pk>/delete/", "post/<int:pk>/update/", "post/new/"
 ["comment/<int:pk>/update/", "post/<int:pk>/comments/new/", "comment/<int:pk>/delete/"]
 ["tags/<slug:tag_slug>/", "PostByTagListView.as_view()"]
+["login/", "register/", "profile/"]
 from django.urls import path
 from . import views
 
