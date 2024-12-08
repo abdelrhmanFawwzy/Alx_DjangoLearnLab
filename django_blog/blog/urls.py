1f055cb570ac330894884644f773fa49f60018a2
abdelrhmanFawwzy <110397608+abdelrhmanFawwzy@users.noreply.github.com>
2024-12-08 21:05:12 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,5 +1,5 @@
 "post/<int:pk>/delete/", "post/<int:pk>/update/", "post/new/"
-
+["comment/<int:pk>/update/", "post/<int:pk>/comments/new/", "comment/<int:pk>/delete/"]
 from django.urls import path
 from . import views
 
