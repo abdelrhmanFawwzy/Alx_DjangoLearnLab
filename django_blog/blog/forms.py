8d5e0cae5f89f8c14ed5d181fadb6ee58ac69974
abdelrhmanFawwzy <110397608+abdelrhmanFawwzy@users.noreply.github.com>
2024-12-09 11:14:38 +0200

    Delete django_blog directory

--- django_blog/blog/forms.py
+++
@@ -1,9 +0,0 @@
- ["TagWidget()", "tags", "widgets"]
-["CommentForm(forms.ModelForm)", "model = Comment", "content"]
-from django.forms import ModelForm
-from . models import *
-
-class login_user_form(ModelForm):
-    class Meta:
-        model = User
-        fields = ['username', 'password']
