891edaef7f54f24d82ef0a44f1f43317c7b16c1c
abdelrhmanFawwzy <110397608+abdelrhmanFawwzy@users.noreply.github.com>
2024-12-14 11:24:27 +0200

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,2 +1,3 @@
 ["viewsets", "viewsets.ModelViewSet", "Comment.objects.all()", "Post.objects.all()"]
 ["Post.objects.filter(author__in=following_users).order_by", "following.all()", "permissions.IsAuthenticated"]
+["generics.get_object_or_404(Post, pk=pk)", "Like.objects.get_or_create(user=request.user, post=post)", "Notification.objects.create"]
