e52ac658361f220c55bfd67dcc59d934b24df337
abdelrhmanFawwzy <110397608+abdelrhmanFawwzy@users.noreply.github.com>
2024-12-14 11:15:55 +0200

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,2 +1,2 @@
-
+["viewsets", "viewsets.ModelViewSet", "Comment.objects.all()", "Post.objects.all()"]
 ["Post.objects.filter(author__in=following_users).order_by", "following.all()", "permissions.IsAuthenticated"]
